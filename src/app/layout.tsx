7c60f875e591d077d213f88da0e3142782c5669a
datnguyen <datnguyen.ute@gmail.com>
2024-10-07 21:07:59 +0700

    Add sample Hompage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import * as React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
-import AppHeader from '@/components/header/app.header';
+import * as React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/theme";
+import AppHeader from "@/components/header/app.header";
+import { LocalizationProvider, AdapterDayjs } from "@/utils/utils";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <AppHeader />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <AppHeader />
+              {props.children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </LocalizationProvider>
       </body>
     </html>
   );
---
+++ src/utils/utils.tsx
@@ -0,0 +1,5 @@
+"use client";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+export { LocalizationProvider, AdapterDayjs };
